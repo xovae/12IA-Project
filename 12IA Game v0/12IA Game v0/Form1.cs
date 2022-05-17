caf502bca9a679371c57681980a79d4cf683046e
xovae <9jboulto@hbhs.school.nz>
2022-05-17 14:59:02 +1200

    12IA Game v0.11
    *actually implement movement, added character placeholder sprite and fundamental game elements (timer, panel etc.)

--- 12IA Game v0/12IA Game v0/Form1.cs
+++ 12IA Game v0/12IA Game v0/Form1.cs
@@ -10,11 +10,57 @@ using System.Windows.Forms;
 
 namespace _12IA_Game_v0
 {
-    public partial class Form1 : Form
+    public partial class frmGame : Form
     {
-        public Form1()
+
+        bool right; //boolean values used for movement (is user pressing direction keys yes/no)
+        bool left;
+        bool up;
+        bool down;
+       
+
+        public frmGame()
         {
             InitializeComponent();
         }
+
+        private void pnlScreen_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tmrMovement_Tick(object sender, EventArgs e)
+        {
+            //side collision
+
+            //top collision
+
+            //side of screen collision
+
+            //movement
+
+            if (right == true) { player.Left += 5; }
+            if (left == true) { player.Left -= 5; }
+            if (up == true) { player.Top -= 5; }
+            if (down == true) { player.Top += 5; }
+
+        }
+        private void frmGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = true; }
+            if (e.KeyCode == Keys.Left) { left = true; }
+            if (e.KeyCode == Keys.Up) { up = true; }
+            if (e.KeyCode == Keys.Down) { down = true; }
+            if (e.KeyCode == Keys.Escape) { this.Close(); }
+        }
+
+        private void frmGame_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = false; }
+            if (e.KeyCode == Keys.Left) { left = false; }
+            if (e.KeyCode == Keys.Up) { up = false; }
+            if (e.KeyCode == Keys.Down) { down = false; }
+        }
+
     }
 }
